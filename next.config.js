d7331f6612963c2d481ccc7fd7446bc9a8cdf628
Awalhossain <waqin21@gmail.com>
2023-10-17 00:45:47 +0600

    chore: Add external site ➕✔️

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['seatgeek.com', 'links.papareact.com', 'res.cloudinary.com'],
+    },
+    env: {
+        API_URL: process.env.API_URL,
+        API_KEY: process.env.API_KEY,
+        API_SECRET: process.env.API_SECRET,
+        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+        NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+    },
+}
 
 module.exports = nextConfig
