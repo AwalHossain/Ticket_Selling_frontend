2f2d3af65f6ecfd6e8fe802422d8f0306ce693b9
Awalhossain <waqin21@gmail.com>
2023-10-17 00:51:22 +0600

    chore: practise data ▶️

---
+++ src/utils/data.ts
@@ -0,0 +1,97 @@
+export const menuLinks = [
+    {
+        name: "Sprots",
+        submenu: true,
+        sublinks: [
+            {
+                name: "Cricket",
+                link: "/",
+            },
+            {
+                name: "Football",
+                link: "/",
+
+            },
+            {
+                name: "Kabaddi",
+                link: "/",
+
+            }
+        ]
+
+    },
+    {
+        name: "Music",
+        submenu: true,
+        sublinks: [
+            {
+                name: "All Concets",
+                link: "/",
+            },
+            {
+                name: "Festivals",
+                link: "/",
+
+            },
+            {
+                name: "Genres",
+                link: "/"
+            }
+        ]
+    },
+    {
+        name: "Shows",
+        submenu: true,
+        sublinks: [
+            {
+                name: "Movies",
+                link: "/",
+            },
+            {
+                name: "Comedy",
+                link: "/",
+
+            },
+            {
+                name: "Theatre",
+                link: "/"
+            }
+        ]
+    },
+    {
+        name: "City",
+        submenu: true,
+        sublinks: [
+            {
+                name: "Dhaka",
+                link: "/",
+            },
+            {
+                name: "Chittagong",
+                link: "/",
+
+            },
+            {
+                name: "Rajshahi",
+                link: "/"
+            },
+            {
+                name: "Khulna",
+                link: "/",
+            },
+            {
+                name: "Mymensingh",
+                link: "/",
+            },
+            {
+                name: "Sylhet",
+                link: "/",
+            },
+            {
+                name: "Barishal",
+                link: "/",
+            }
+        ]
+    }
+
+] 
