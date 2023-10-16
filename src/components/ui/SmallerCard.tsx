8fa0acd2c9369f96ff9f4df1eefb7398d7d81eef
Awalhossain <waqin21@gmail.com>
2023-10-17 00:48:23 +0600

    feat: smaller card intialize 👌

---
+++ src/components/ui/SmallerCard.tsx
@@ -0,0 +1,157 @@
+'use client';
+
+import Image from "next/image";
+
+// image,
+//     title,
+//     date,
+//     venue,
+// : SmallCardProps
+
+const SmallerCard = () => {
+    return (
+        <>
+
+            <div className=" flex items-center m-2 mt-5 space-x-8 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl
+        cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out
+        ">
+                {/* Left side */}
+                <div className="relative h-12 w-16">
+                    <Image src={`https://seatgeek.com/_next/image?url=https%3A%2F%2Fseatgeek.com%2Fimages%2Fperformers-landscape%2Fnascar-cup-series-1-737f19%2F607605%2F1500x2000.jpg&w=1920&q=75`}
+                        layout="fill"
+                        alt="NASCAR Cup Series"
+                    />
+
+                </div>
+
+                {/* Right side */}
+                <div>
+                    <h2>NASCAR at HMS(Contender Boats)(2-day)</h2>
+                    <h3 className="text-gray-500">
+                        <span>$35</span>
+                        <span>
+                            Homestead-Miami Speedway. Homestead, FL
+                        </span>
+                    </h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SmallerCard
