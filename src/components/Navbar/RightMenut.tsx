80e7d4acc44f5feb6b10f67199f30ae47db22f8d
Awalhossain <waqin21@gmail.com>
2023-10-17 18:59:44 +0600

    feat: Add modal for registration 📝

--- src/components/Navbar/RightMenut.tsx
+++ src/components/Navbar/RightMenut.tsx
@@ -1,13 +1,52 @@
 'use client'
-const Profile = () => {
-    return (
 
-        <button className="bg-blue-950 text-black px-6 py-2
+
+import { Modal } from 'antd';
+import React, { useState } from 'react';
+
+const Profile: React.FC = () => {
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    const [modalText, setModalText] = useState('Content of the modal');
+
+    const showModal = () => {
+        setOpen(true);
+    };
+
+    const handleOk = () => {
+        setModalText('The modal will be closed after two seconds');
+        setConfirmLoading(true);
+        setTimeout(() => {
+            setOpen(false);
+            setConfirmLoading(false);
+        }, 2000);
+    };
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setOpen(false);
+    };
+
+    return (
+        <>
+
+            <button onClick={showModal} className="bg-blue-950 text-black px-6 py-2
              rounded-full
              ">
-            Get Started
-        </button>
-    )
-}
+                Get Started
+            </button>
 
-export default Profile
+            <Modal
+                title="Title"
+                open={open}
+                onOk={handleOk}
+                confirmLoading={confirmLoading}
+                onCancel={handleCancel}
+            >
+                <p>{modalText}</p>
+            </Modal>
+        </>
+    );
+};
+
+export default Profile;
