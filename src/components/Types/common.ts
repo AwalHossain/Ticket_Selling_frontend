abfb237120b6fc0e55b9bf6a254f5d982bb8d023
Awalhossain <waqin21@gmail.com>
2023-10-18 18:39:14 +0600

    feat: Add common types ✅

--- src/components/Types/common.ts
+++ src/components/Types/common.ts
@@ -4,3 +4,26 @@ export interface SmallCardProps {
     date: string;
     venue: string;
 }
+
+
+export interface IMeta {
+    limit: number;
+    page: number;
+    total: number;
+}
+
+export type ResponseSuccessType = {
+    data: any;
+    meta?: IMeta;
+};
+
+export type IGenericErrorResponse = {
+    statusCode: number;
+    message: string;
+    errorMessages: IGenericErrorMessage[];
+};
+
+export type IGenericErrorMessage = {
+    path: string | number;
+    message: string;
+};
---
+++ src/components/Types/index.ts
@@ -0,0 +1,3 @@
+// export common.ts from this file , give me the code
+
+export * as common from './common';
