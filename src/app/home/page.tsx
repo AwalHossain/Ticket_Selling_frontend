29f50562688ef4193e71b33f3874171451911d8e
Awalhossain <waqin21@gmail.com>
2023-10-22 03:48:22 +0600

    refactor: file changed ❌📝

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Header from "@/components/Header"
+import Header from "@/components/headers/Header"
 import Home from "@/components/Home/HomePage"
 import Trending from "@/components/Trending"
 
